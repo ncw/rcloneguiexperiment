ae18f7d7a87158e6ef9c6119117d93030512805c
Nick Craig-Wood <nick@craig-wood.com>
2018-11-07 11:18:42 +0000

    Fix formatting to 2 space indents

--- src/App.js
+++ src/App.js
@@ -3,83 +3,83 @@ import './App.css';
 import { rclone } from './rclone.js';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            method: "operations/list",
-            input: '{ "fs": "/tmp", "remote": "", "opt":{} }',
-            output: 'RPC output goes here',
-            help: [{
-                Help: "list",
-                Path: "operations/list",
-                Title:" list",
-            }],
-            helpByPath: {},
-            failed: false,
-        };
-        this.getHelp();
+  constructor(props) {
+    super(props);
+    this.state = {
+      method: "operations/list",
+      input: '{ "fs": "/tmp", "remote": "", "opt":{} }',
+      output: 'RPC output goes here',
+      help: [{
+        Help: "list",
+        Path: "operations/list",
+        Title:" list",
+      }],
+      helpByPath: {},
+      failed: false,
+    };
+    this.getHelp();
+  }
+  getHelp() {
+    rclone.rpc("rc/list", {}).then(response => {
+      var byPath = {};
+      response.commands.forEach(x => {
+        byPath[x.Path] = x.Help;
+      });
+      this.setState({
+        help: response.commands,
+        helpByPath: byPath,
+      });
+      this.setMethod("operations/list");
+    }).catch(error => {
+      console.log("failed to read rc/help", error);
+    });
+  }
+  doRPC() {
+    console.log("doRPC");
+    var input;
+    try {
+      input = JSON.parse(this.state.input);
+    } catch(err) {
+      this.setState({
+        output: "Failed to parse JSON input: " + err.message,
+      });
+      return;
     }
-    getHelp() {
-        rclone.rpc("rc/list", {}).then(response => {
-            var byPath = {};
-            response.commands.forEach(x => {
-                byPath[x.Path] = x.Help;
-            });
-            this.setState({
-                help: response.commands,
-                helpByPath: byPath,
-            });
-            this.setMethod("operations/list");
-        }).catch(error => {
-            console.log("failed to read rc/help", error);
-        });
-    }
-    doRPC() {
-        console.log("doRPC");
-        var input;
-        try {
-            input = JSON.parse(this.state.input);
-        } catch(err) {
-            this.setState({
-                output: "Failed to parse JSON input: " + err.message,
-            });
-            return;
-        }
-        console.log(this.state.method);
-        console.log(input);
-        rclone.rpc(this.state.method, input).then(response => {
-            var result = JSON.stringify(response, null, 4);
-            console.log("success", result);
-            this.setState({
-                output: result,
-                failed: false,
-            });
-        }).catch(error => {
-            var result = JSON.stringify(error, null, 4);
-            this.setState({
-                output: "FAILED: " + result,
-                failed: true,
-            });
-        });
-    }
-    setMethod(method) {
-        this.setState({
-            method: method,
-            output: this.state.helpByPath[method],
-            failed: false,
-        });
-    }
-    render() {
-        var methods = this.state.help.map(v => <option value={v.Path} key={v.Path}>{v.Path}: {v.Title}</option>);
-        return (
-            <div className="App">
-              <select onChange={(event) => this.setMethod(event.target.value)}>{ methods }</select>
-              <textarea rows="10" cols="50" value={this.state.input} onChange={(event) => this.setState({input: event.target.value})}></textarea>
-              <button onClick={() => this.doRPC()}>Run</button>
-              <pre className={ this.state.failed ? "failed" : ""}>{this.state.output}</pre>
-            </div>
-        );
-    }
+    console.log(this.state.method);
+    console.log(input);
+    rclone.rpc(this.state.method, input).then(response => {
+      var result = JSON.stringify(response, null, 4);
+      console.log("success", result);
+      this.setState({
+        output: result,
+        failed: false,
+      });
+    }).catch(error => {
+      var result = JSON.stringify(error, null, 4);
+      this.setState({
+        output: "FAILED: " + result,
+        failed: true,
+      });
+    });
+  }
+  setMethod(method) {
+    this.setState({
+      method: method,
+      output: this.state.helpByPath[method],
+      failed: false,
+    });
+  }
+  render() {
+    var methods = this.state.help.map(v => <option value={v.Path} key={v.Path}>{v.Path}: {v.Title}</option>);
+    return (
+      <div className="App">
+        <select onChange={(event) => this.setMethod(event.target.value)}>{ methods }</select>
+        <textarea rows="10" cols="50" value={this.state.input} onChange={(event) => this.setState({input: event.target.value})}></textarea>
+        <button onClick={() => this.doRPC()}>Run</button>
+        <pre className={ this.state.failed ? "failed" : ""}>{this.state.output}</pre>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/rclone.js
+++ src/rclone.js
@@ -1,43 +1,43 @@
 // Class for accessing rclone
 class Rclone {
-    constructor() {
-        this.url = window.location.href;
-        // If using the react test server use the default rc URL
-        if (window.location.host === "localhost:3000") {
-            this.url = "http://127.0.0.1:5572/";
-            console.log("React dev server detected - using the default rc URL:", this.url);
+  constructor() {
+    this.url = window.location.href;
+    // If using the react test server use the default rc URL
+    if (window.location.host === "localhost:3000") {
+      this.url = "http://127.0.0.1:5572/";
+      console.log("React dev server detected - using the default rc URL:", this.url);
+    }
+    this.headers = {
+      "Content-Type": "application/json"
+    };
+  }
+  // Do an RPC to rclone - input should be an object
+  //
+  // This returns a promise which will return an object
+  rpc(method, input) {
+    var url = this.url + method;
+    return fetch(url, {
+      method: "POST",
+      // mode: "same-origin",
+      // credentials: "same-origin",
+      headers: this.headers,
+      body: JSON.stringify(input)
+    }).then(response => {
+      var ok = response.ok;
+      return response.json().then(result => {
+        if (!ok) {
+          console.log("FAILED" + result);
+          return Promise.reject(result);
         }
-        this.headers = {
-            "Content-Type": "application/json"
-        };
-    }
-    // Do an RPC to rclone - input should be an object
-    //
-    // This returns a promise which will return an object
-    rpc(method, input) {
-        var url = this.url + method;
-        return fetch(url, {
-            method: "POST",
-            // mode: "same-origin",
-            // credentials: "same-origin",
-            headers: this.headers,
-            body: JSON.stringify(input)
-        }).then(response => {
-            var ok = response.ok;
-            return response.json().then(result => {
-                if (!ok) {
-                    console.log("FAILED" + result);
-                    return Promise.reject(result);
-                }
-                return result;
-            });
-        }, error => {
-            console.error('Rclone RPC error: ' + error);
-            return Promise.reject({
-                error: String(error),
-            });
-        });
-    }
+        return result;
+      });
+    }, error => {
+      console.error('Rclone RPC error: ' + error);
+      return Promise.reject({
+        error: String(error),
+      });
+    });
+  }
 }    
 
 export var rclone = new Rclone();
